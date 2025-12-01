d636dfa51f8b144badd1be3b4b6c247029f9b3d2
Deepanshu072001 <deepanshuraturi1107@outlook.com>
2025-12-01 17:02:10 +0530

    commited app.js file

--- src/app.js
+++ src/app.js
@@ -21,8 +21,11 @@ app.use(cors({
 //import the routes
 
 import healthCheckRouter from "./routes/healthcheck.route.js";
+import authRouter from "./routes/auth.route.js"
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", authRouter);
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to project management");
